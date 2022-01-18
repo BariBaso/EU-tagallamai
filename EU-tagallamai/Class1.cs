7ddc2ff491da533d5ba902a8fcfb816aaf76f78b
BariBaso <kissmarci41@gmail.com>
2022-01-18 10:41:04 +0100

    1.-3. feladat

---
+++ EU-tagallamai/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EU_tagallamai
+{
+    class Class1
+    {
+        public string név { get; set; }
+        public DateTime dátum { get; set; }
+
+
+        public Class1(string sor)
+        {
+            string[] t = sor.Split(';');
+            név = t[0];
+            dátum = Convert.ToDateTime(t[1]);
+        }
+    }
+}
---
+++ EU-tagallamai/EU.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EU_tagallamai
+{
+    class EU
+    {
+        static void Main(string[] args)
+        {
+            List<Class1> tagok = new List<Class1>();
+            foreach (var x in File.ReadAllLines("EUcsatlakozas.txt"))
+            {
+                tagok.Add(new Class1(x));
+            }
+
+            //3.
+            Console.WriteLine($"EU tagállamainak száma: {tagok.Count}");
+
+        }
+    }
+}
