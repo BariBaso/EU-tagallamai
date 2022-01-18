f37e127ffde4d1ff13067f9078c42bd700d3858c
BariBaso <kissmarci41@gmail.com>
2022-01-18 10:18:42 +0100

    Add project files.

---
+++ EU-tagallamai/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EU_tagallamai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
